7e6615ccc7ee613783d0fba1daa000f714e28bdf
zetro-malik <zeeshanimsb@gmail.com>
2023-09-30 20:16:27 +0500

    add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/about'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div className='flex '>
-      <h1 className="text-8xl font-bold underline hover:text-red-400">
-      Hello world!
-    </h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+    <Routes>SignIn
+    <Route path='/' element={<Home/>}/>
+    <Route path='/sign-in' element={<SignIn/>}/>
+    <Route path='/sign-up' element={<SignUp/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='text-4xl font-medium'>Home</div>
+  )
+}
