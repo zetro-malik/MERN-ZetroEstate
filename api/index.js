93c55a3db39b3bfbad80980eb85fe91ee4ac2e1e
zetro-malik <zeeshanimsb@gmail.com>
2023-09-30 21:38:22 +0500

    connect mongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import { exit } from 'process';
+
+dotenv.config();
+
+ 
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log('mongo running ok')
+}).catch((e)=>{
+    console.log('some error occue closing the backend!!!')
+    process.exit(1);
+})
 
 const app = express();
 
