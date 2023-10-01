6474f54b8ab944a1dc27cbad14a57b0e2cfe01a9
zetro-malik <zeeshanimsb@gmail.com>
2023-10-01 11:10:17 +0500

    complete signup with react-toast

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,22 +1,73 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import { toast, ToastContainer } from 'react-toastify';
 
 export default function SignUp() {
+
+  const navigate = useNavigate();
+
+  const [formData , setFormData] = useState({})
+  const [loading , setLoading] = useState(false)
+
+  const handleChange = (e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id ]: e.target.value
+    })
+  }
+  
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    setLoading(true);
+    try{
+      const res = await fetch('/api/auth/signup',{
+        method:"POST",
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify(formData)
+      });
+      const data = await res.json();
+      if(data.success === false){
+        toast.error(data.message, {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      console.log(data);
+      toast.success("User Created", {
+        position: toast.POSITION.TOP_RIGHT
+      });
+      navigate('/sign-in')
+    }catch(e){
+      toast.error(e.message, {
+        position: toast.POSITION.TOP_RIGHT
+      });
+        setLoading(false);
+        setError(e.message);
+      
+    }
+  }
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4' >
-      <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
-      <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' />
-        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' />
-      <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
-      </form>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit} >
+      <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username'  onChange={handleChange}/>
+      <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email'onChange={handleChange} />
+        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password'onChange={handleChange} />
+      <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? "Loading..." : "Sign Up"}</button>
+      </form >
       <div className='flex gap-2 mt-5 '>
         <p>Have an account?</p>
         <Link to="/sign-in">
           <span className='text-blue-700'>Sign In</span>
         </Link>
       </div>
+      <ToastContainer/>
     </div>
   )
 }
